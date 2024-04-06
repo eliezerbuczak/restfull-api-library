273760771c51bbcd25a2d9c7fa515a1b50886141
Eliezer Boeira <eliezerbuczak2016@gmail.com>
2024-04-06 19:42:29 -0300

    book repository

--- app/Repositories/Eloquent/BookRepository.php
+++ app/Repositories/Eloquent/BookRepository.php
@@ -2,9 +2,13 @@
 
 namespace App\Repositories\Eloquent;
 
+use App\Models\Book;
 use App\Repositories\Interfaces\BookRepositoryInterface;
 
 class BookRepository extends BaseRepository implements BookRepositoryInterface
 {
-
+    public function __construct()
+    {
+        $this->model = new Book();
+    }
 }
