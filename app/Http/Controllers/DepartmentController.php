4983a7b9b57854ad673f4e2546c1a65ae62c7cff
Eliezer Boeira <eliezerbuczak2016@gmail.com>
2024-04-07 14:17:52 -0300

    try catch Department

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -17,6 +17,12 @@ class DepartmentController extends Controller
     public function index(): \Illuminate\Http\JsonResponse
     {
         $departments = $this->department->all();
+        if (isset($departments['message'])) {
+            return response()->json($departments, 404);
+        }
+        if(isset($departments['error'])){
+            return response()->json($departments, 500);
+        }
         return response()->json($departments);
     }
 
@@ -24,12 +30,21 @@ class DepartmentController extends Controller
     {
         $data = $request->all();
         $department = $this->department->create($data);
+        if(isset($department['error'])){
+            return response()->json($department, 500);
+        }
         return response()->json($department);
     }
 
     public function show($id): \Illuminate\Http\JsonResponse
     {
         $department = $this->department->find($id);
+        if (isset($department['message'])) {
+            return response()->json($department, 404);
+        }
+        if(isset($department['error'])){
+            return response()->json($department, 500);
+        }
         return response()->json($department);
     }
 
@@ -37,12 +52,24 @@ class DepartmentController extends Controller
     {
         $data = $request->all();
         $department = $this->department->update($id, $data);
+        if(isset($department['message'])){
+            return response()->json($department, 404);
+        }
+        if(isset($department['error'])){
+            return response()->json($department, 500);
+        }
         return response()->json($department);
     }
 
     public function destroy($id): \Illuminate\Http\JsonResponse
     {
         $department = $this->department->delete($id);
+        if(isset($department['message'])){
+            return response()->json($department, 404);
+        }
+        if(isset($department['error'])){
+            return response()->json($department, 500);
+        }
         return response()->json($department);
     }
 }
